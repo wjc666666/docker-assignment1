b8ea7fb61cf5922f61ed81d05355cd93c23e26a8
wjc666666 <wangjunchengdsb@gmail.com>
2024-11-04 15:40:01 +0000

    adding the mongo-seed

--- seed.js
+++ seed.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 const seedingData = require('./seeding.json');
 
+const mongoURI = process.env.MONGODB_URI || 'mongodb://admin:password@mongo:27017/tmdb_movies';
 
-mongoose.connect('mongodb://admin:password@mongo:27017/tmdb_movies', {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  user: 'admin',
-  pass: 'password',
-  authSource: 'admin'
 });
 
 const movieSchema = new mongoose.Schema({}, { strict: false });
@@ -15,15 +13,12 @@ const Movie = mongoose.model('Movie', movieSchema);
 
 async function seedDatabase() {
   try {
-  
     await Movie.deleteMany({});
-
     const result = await Movie.insertMany(seedingData);
     console.log('Data seeded:', result);
   } catch (error) {
     console.error('Error seeding data:', error);
   } finally {
-
     mongoose.connection.close();
   }
 }
